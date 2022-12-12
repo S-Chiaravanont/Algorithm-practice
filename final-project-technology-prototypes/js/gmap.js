bbcb9a0ea0c92dbba02162e12cda79d84934a9d5
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-12-12 17:37:43 +0000

    in progress, incorporate Gmap api to generate interactive google maps for demo

---
+++ final-project-technology-prototypes/js/gmap.js
@@ -0,0 +1,18 @@
+// function initMap() {
+//   let map;
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 14
+//   });
+
+//   const marker = new google.maps.Marker({
+//     position: { lat: -34.397, lng: 150.644 },
+//     map: map;
+//   });
+
+//   const detailWindow = new google.maps.InfoWindow({
+//     content: ``
+//   })
+// }
+
+// window.initMap = initMap;
